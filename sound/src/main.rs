ddfbe375af477015c72740c39fec521e5b9e10ce
Thibault Cheneviere <thibault.chene23@gmail.com>
2025-07-13 02:58:12 +0200

    feat: added a way to select the device and if the device is invalid go back to default mode

--- sound/src/main.rs
+++ sound/src/main.rs
@@ -2,6 +2,7 @@ use std::io::Read;
 use std::io::Seek;
 use std::time::Duration;
 
+use rodio::Device;
 use rodio::DeviceTrait;
 use rodio::cpal;
 use rodio::cpal::traits::HostTrait;
@@ -14,22 +15,31 @@ enum Command {
 }
 
 impl Command {
-    fn execute(&self, sink: &Sink, state: &mut State) {
+    fn execute(&self, sink: &Option<Sink>, state: &mut State) {
         match self {
             Self::Interval(val) => state.interval = *val,
             Self::Sleep(val) => {
                 let source = SineWave::new(440.0)
                     .take_duration(Duration::from_secs_f32(*val))
                     .amplify(0.0);
-                sink.append(source);
+
+                if let Some(sink) = sink {
+                    sink.append(source);
+                } else {
+                    println!("[DEBUG] - sleeping for {}", *val);
+                }
             }
             Self::Note(_) => {
                 // SAFETY: this is safe to unwrap because a note command always return a frequency
                 let frequency = self.calc_freq().unwrap();
-                println!("frequency played: {frequency}");
                 let source =
                     SineWave::new(frequency).take_duration(Duration::from_secs_f32(state.interval));
-                sink.append(source);
+
+                if let Some(sink) = sink {
+                    sink.append(source);
+                } else {
+                    println!("[DEBUG] - playing freq {frequency}");
+                }
             }
         }
     }
@@ -85,7 +95,7 @@ fn main() {
     // This is formated like so ...SOUND_COMMAND\nSOUND_COMMAND_SIZE
     // SOUND_COMMAND_SIZE is exactly 8 bytes long make is possible to have a size of 99_999_999 (deemed large enough)
     let mut buf: [u8; 8] = [0; 8];
-    file.read(&mut buf).unwrap();
+    file.read_exact(&mut buf).unwrap();
     file.rewind().unwrap();
 
     let sound_command_size = str::from_utf8(&buf)
@@ -107,19 +117,46 @@ fn main() {
     let mut state = State { interval: 0.0 };
 
     let host = cpal::default_host();
+    let devices: Vec<Device> = host.devices().unwrap().collect();
 
-    #[cfg(not(target_os = "macos"))]
-    let device = host.default_output_device().unwrap();
+    println!("Select a device to use (enter it's number):");
+    devices.iter().enumerate().for_each(|(i, d)| {
+        println!(
+            "{} - {}",
+            i + 1,
+            d.name().unwrap_or("no name found".to_string())
+        )
+    });
 
-    #[cfg(target_os = "macos")]
-    let device = host
-        .devices()
-        .unwrap()
-        .find(|d| d.name().unwrap() == "MacBook Air Speakers")
-        .unwrap();
+    let mut input_device: [u8; 1] = [0u8; 1];
+    std::io::stdin()
+        .read_exact(&mut input_device)
+        .expect("unable to read from stdin");
+    let input_device = str::from_utf8(&input_device).expect("invalid device index");
+    let input_device_index = input_device.parse::<usize>().expect("invalid device index");
 
-    let (_, stream_handle) = OutputStream::try_from_device(&device).unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+    // Select the input device by name
+    let device = devices
+        // - 1 is used because for display purpose we added 1
+        .get(input_device_index - 1)
+        .expect("invalid device index");
+
+    println!(
+        "[INFO]  - device selected: {}",
+        device.name().unwrap_or("no name found".to_string())
+    );
+
+    let sink = match OutputStream::try_from_device(device) {
+        Ok((_, stream_handle)) => {
+            Some(Sink::try_new(&stream_handle).expect("cannot create a sink"))
+        }
+        Err(_) => {
+            println!(
+                "[ERROR] - unable to create an output stream from the device defaulting to debug mode"
+            );
+            None
+        }
+    };
 
     // We parse the command in order to utilize each command
     sound_command
@@ -136,5 +173,7 @@ fn main() {
         })
         .for_each(|c| c.execute(&sink, &mut state));
 
-    sink.sleep_until_end();
+    if let Some(sink) = sink {
+        sink.sleep_until_end();
+    }
 }
